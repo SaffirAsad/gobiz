2e86958977f0bf25b4e63c374cbdb577cf2f79d8
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-02-16 21:46:07 +0100

    Update detectPhone.js

--- js/detectPhone.js
+++ js/detectPhone.js
@@ -1,7 +1,10 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   //console.log("This website is loaded on a phone.");
   document.querySelectorAll("select").forEach(select=>{
-    select.style.width="90%";
+    select.style.width="48%";
     select.style.marginTop="10px";
   });
+  document.querySelector("#filterButton").style.marginTop="10px";
+  document.querySelector("#filterButton").style.width="80%";
+  
 }
