deeeb10d5ae700c761915d9a5c752a84cb32f86d
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-02-17 01:20:51 +0100

    Update detectPhone.js

--- js/detectPhone.js
+++ js/detectPhone.js
@@ -7,4 +7,8 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   document.querySelector("#selecter").style.margin="10px 0px 10px 0";
   document.querySelector("#filterButton").style.margin="10px 0px 10px 0";
   document.querySelector("#filterButton").style.width="100%";
+  
+  document.querySelector("body > div > section:nth-child(1) > nav > div > div").style.width = "50%"
+  document.querySelector("body > div > section:nth-child(1) > nav > div > div > button > span > svg").style.marginLeft="12.5px"
+  document.querySelector("body > div > section:nth-child(1) > nav > div > div > button > span > svg").style.width = "100%"
 }
