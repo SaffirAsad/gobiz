3844e727d74e0312d6ac23fa3cb6734de0324c2f
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-02-23 01:24:57 +0100

    Update FilterPrice.js

--- js/FilterPrice.js
+++ js/FilterPrice.js
@@ -45,36 +45,3 @@ function Search(products){
     ProductGrid(FiltedProducts,headers,price.min, price.max)
 }
 
-// slider test
-var sliderTrack = document.querySelector("div.slider-track");
-//sliderTrack.innerHTML = `<img class="imgSL" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg">`;
-//sliderTrack.innerHTML = `<img class="imgSL" src="https://images.pexels.com/photos/13270287/pexels-photo-13270287.jpeg">`;
-sliderTrack.innerHTML = `<img src="https://cdn.pixabay.com/photo/2022/11/04/19/17/alone-7570547_640.jpg">`;
-
-
-fixedDiv=document.querySelector("div.slider-track");
-imgs=document.querySelectorAll("div.slider-track>img");
-fixedDiv.style.backgroundColor="gray";
-setTimeout(()=>{
-  imgs.forEach(img=>{
-    w=img.width;
-    h=img.height;
-    console.log(w,h);
-    if (w>h){
-      img.style.width="300px";
-      img.style.height="auto";
-      if(h!=0){
-        img.style.marginTop = `${parseInt((300-img.height)/2)}px`;
-      }
-    }else{
-      img.style.width="auto";
-      img.style.height="300px"; 
-      if(w!=0){
-        img.style.marginLeft = `${parseInt((300-img.width)/2)}px`;
-      }
-    }
-  });
-  fixedDiv.style.width="300px";
-  fixedDiv.style.height="300px";
-},0);
-
