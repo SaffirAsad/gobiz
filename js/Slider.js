e610ae77f1af4299d75953a671e26fb10ea447db
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-02-23 03:15:43 +0100

    Update Slider.js

--- js/Slider.js
+++ js/Slider.js
@@ -69,6 +69,7 @@ elementsOnhover.forEach(
 fixedDiv=document.querySelector("div.slider-track");
 imgs=document.querySelectorAll("div.slider-track>img");
 //fixedDiv.style.backgroundColor="gray";
+/*
 setTimeout(()=>{
   imgs.forEach(img=>{
     w=img.width;
@@ -91,3 +92,4 @@ setTimeout(()=>{
   fixedDiv.style.width="300px";
   fixedDiv.style.height="300px";
 },0);
+*/
