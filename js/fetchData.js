f48ea83a59bec2d40c9a6aedd36c18fce744cc54
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-01 14:09:37 +0100

    commit5

--- js/fetchData.js
+++ js/fetchData.js
@@ -180,6 +180,7 @@ function AddDel(el,products){
     session_subCat.append(SubCategories);
     shop.append(section_products);
     shop.append(section_category);
+    shop.append(section_products);
 };
 function AddDelsub(subCatel,products){
     CatName=subCatel.querySelector("#Cat").textContent
