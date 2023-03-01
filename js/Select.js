c85dadd80e6a294cc4ed4649876028c711fa25cc
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-01 23:50:17 +0000

    v41

--- js/Select.js
+++ js/Select.js
@@ -7,19 +7,17 @@ function getUniqueValuesForKey(array, key, value, returnKey) {
         )
     );
 }
-function MenuUser(products, headers){
+function MenuUser(products){
     //console.log("MenuUser");
     const ownerSelect = document.getElementById("owner-select");
     const mainCategorySelect = document.getElementById("mainCategorySelect");
     const subCategorySelect = document.getElementById("subCategorySelect");
     const sizeSelect = document.getElementById("sizeSelect");
     const colorSelect = document.getElementById("colorSelect");
-    var	MainCategory = Object.values(headers)[2];	
-    var SubCategory = Object.values(headers)[3];	
-    var price = Object.values(headers)[5];
-    var Size = Object.values(headers)[7];	
-    var Color = Object.values(headers)[8];
-
+    var	mMainCategory = Object.values(headers)[2];	
+    var mSubCategory = Object.values(headers)[3];
+    var mSize = Object.values(headers)[7];	
+    var mColor = Object.values(headers)[8];
 
     function setOptions(element,Title,SelectList){
         let CodeHTML = '';
@@ -27,8 +25,8 @@ function MenuUser(products, headers){
         element.innerHTML =  Title + CodeHTML;
     }
     /* Main category */
-    let mainCategory = products.map(obj => obj[MainCategory]);
-    SelectTitle  = `<option value="">Main Category</option>`;
+    let mainCategory = products.map(obj => obj[mMainCategory]);
+    SelectTitle  = `<option value="">${mMainCategory}</option>`;
     setOptions(mainCategorySelect,SelectTitle,[...new Set(mainCategory)]);
 
     
@@ -36,20 +34,20 @@ function MenuUser(products, headers){
         const selectedMainCategory = this.value;
         subCategorySelect.innerHTML = "<option value=''>Sub Category</option>";
         subCategorySelect.disabled = true;
-        sizeSelect.innerHTML = "<option value=''>Size</option>";
+        sizeSelect.innerHTML = `<option value=''>${mSize}</option>`;
         sizeSelect.disabled = true;
-        colorSelect.innerHTML = "<option value=''>Color</option>";
+        colorSelect.innerHTML = `<option value=''>${mColor}</option>`;
         colorSelect.disabled = true;
         filterButton.disabled = true;
         if (selectedMainCategory) {
             const subCategories = getUniqueValuesForKey(
                 products,
-                MainCategory,
+                mMainCategory,
                 selectedMainCategory,
-                SubCategory
+                mSubCategory
             );
             subCategorySelect.innerHTML =
-                "<option value=''>Sub Category</option>" +
+                `<option value=''>${mSubCategory}</option>` +
                 subCategories
                 .map(
                     this_subCategory =>
@@ -62,21 +60,21 @@ function MenuUser(products, headers){
 
     subCategorySelect.addEventListener("change", function() {
         const selectedSubCategory = this.value;
-        sizeSelect.innerHTML = "<option value=''>Size</option>";
+        sizeSelect.innerHTML = `<option value=''>${mSize}</option>`;
         sizeSelect.disabled = true;
-        colorSelect.innerHTML = "<option value=''>Size</option>";
+        colorSelect.innerHTML = `<option value=''>${mColor}</option>`;
         colorSelect.disabled = true;
         filterButton.disabled = true;
         if (selectedSubCategory) {
-            const Sizes = getUniqueValuesForKey(
+            const Size = getUniqueValuesForKey(
                 products,
-                SubCategory,
+                mSubCategory,
                 selectedSubCategory,
-                Size
+                mSize
             );
             sizeSelect.innerHTML =
-                "<option value=''>Size</option>" +
-                Sizes
+                `<option value=''>${mSize}</option>` +
+                Size
                 .map(size => `<option value='${size}'>${size}</option>`)
                 .join("");
             sizeSelect.disabled = false;
@@ -84,18 +82,18 @@ function MenuUser(products, headers){
     });
     sizeSelect.addEventListener("change", function() {
         const selectedSize = this.value;
-        colorSelect.innerHTML = "<option value=''>Color</option>";
+        colorSelect.innerHTML = `<option value=''>${mColor}</option>`;
         colorSelect.disabled = true;
         filterButton.disabled = true;
         if (selectedSize) {
             const colors = getUniqueValuesForKey(
                 products,
-                Size,
+                mSize,
                 selectedSize,
-                Color
+                mColor
             );
             colorSelect.innerHTML =
-                "<option value=''>Color</option>" +
+                `<option value=''>${mColor}</option>` +
                 colors
                 .map(color => `<option value='${color}'>${color}</option>`)
                 .join("");
@@ -105,8 +103,4 @@ function MenuUser(products, headers){
     colorSelect.addEventListener("change", function() {
         filterButton.disabled = !this.value;
     });
-    
-
 }
-
-
