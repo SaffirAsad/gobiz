5504b9abc1e5131ed6f4dace069d3f7dbeb34bac
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-07 22:32:14 +0000

    as

--- js/fetchData.js
+++ js/fetchData.js
@@ -218,7 +218,7 @@ function AddDel(el,products){
     shop.append(section_products);
 
     // scroll to the sub categories session
-    scrollFromTo(".Categories",".SubCategories")
+    Interval = setInterval(()=>{scrollFromTo(".SubCategories",".Filtred-Products");clearInterval(Interval)},500)
     
 };
 function AddDelsub(subCatel,products){
@@ -319,7 +319,7 @@ function AddDelsub(subCatel,products){
     shop.append(section_products);
     shop.append(session_subCat);
     shop.append(section_category);
-    scrollFromTo(".SubCategories",".Filtred-Products")
+    Interval = setInterval(()=>{scrollFromTo(".SubCategories",".Filtred-Products");clearInterval(Interval)},500) 
 }
 function scrollFromTo(FromEl, ToEl){
     $(document).ready(function() {
