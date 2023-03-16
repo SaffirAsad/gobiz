8bbb44f3ed5484f34a5c68b990c57525bd7a7b8c
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-17 00:35:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-document.getElementById("year").innerHTML=new Date().getFullYear();function successAlert(msg){"use strict";Swal.fire({toast:true,position:'top-end',icon:'success',title:msg,showConfirmButton:false,timer:1500})}
+document.getElementById("year").innerHTML=new Date().getFullYear();
+function successAlert(msg) {
+    "use strict";
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        icon: 'success',
+        title: msg,
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+function successFavAlert(msg) {
+    "use strict";
+    Swal.fire({
+        toast: true,
+        position: 'top-left',
+        icon: 'success',
+        title: msg,
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
