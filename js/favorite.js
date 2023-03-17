3d06d152f9081bf8b16462e5c328b03129f82280
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-17 14:41:21 -0700

    e

--- js/favorite.js
+++ js/favorite.js
@@ -1,5 +1,5 @@
 const favorite = (id) => {
-    console.log("ClassList",$(`#${id}_favorite`).classList);
+    console.log("ClassList",document.querySelector(`#${id}_favorite`).classList);
     addToFavCart(id);
     id+="_favorite"
     const heartIcon = document.getElementById(id);
