8f15b7c786794cf9c15880a6efd947a2868ca524
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-03-22 00:22:27 +0000

    xz

--- js/favorite.js
+++ js/favorite.js
@@ -47,4 +47,4 @@ const favorite = (id) => {
 
 //update favorite products based on saved cookies  
 window.onload = checkFavorite();
-window.onload  = updateFavListOnload(products)
+setTimeout(()=>{updateFavListOnload(products)},5000) 
