7cdb3a59d8b9aa21cfb030417f43d7741917163b
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-04-02 16:36:28 +0000

    xcv

--- js/paypal.js
+++ js/paypal.js
@@ -1,43 +1,44 @@
 $('label').on('click', function(){
-var color = $(this).next().css('color');
-console.log(color);
-if (color == 'rgb(177, 177, 177)') {
-    $(this).next().addClass('grey');
-}
-else { 
-    $(this).next().removeClass('grey');
-}
+    var color = $(this).next().css('color');
+    console.log(color);
+    if (color == 'rgb(177, 177, 177)') {
+        $(this).next().addClass('grey');
+    }
+    else { 
+        $(this).next().removeClass('grey');
+    }
 });
 
 function initPayPalButton() {
-var amount = 100;// document.querySelector('#mainTotalPrice').querySelector(".total").innerHTML;
-paypal.Buttons({
-    style: {
-    shape: 'rect',
-    color: 'gold',
-    layout: 'vertical',
-    label: 'paypal',
-    },
-    createOrder: function(data, actions) {
-    return actions.order.create({
-        purchase_units: [{"amount":{"currency_code":"USD","value": amount}}]
-    });
-    },
-    onApprove: function(data, actions) {
-    return actions.order.capture().then(function(orderData) {
-        // Full available details
-        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-        // Show a success message within this page, e.g.
-        const element = document.getElementById('paypal-button-container');
-        element.innerHTML = '';
-        element.innerHTML = '<h3>Thank you for your payment!</h3>';
-        // Or go to another URL:  actions.redirect('thank_you.html');
-    });
-    },
-    onError: function(err) {
-    console.log(err);
-    }
-}).render('#paypal-button-container');
+    var amount = document.querySelector('#mainTotalPrice').querySelector(".total").innerHTML;
+    paypal.Buttons({
+        style: {
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'paypal',
+        },
+        createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{"amount":{"currency_code":"USD","value": amount}}]
+        });
+        },
+        onApprove: function(data, actions) {
+        return actions.order.capture().then(function(orderData) {
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+        },
+        onError: function(err) {
+        console.log(err);
+        }
+    }).render('#paypal-button-container');
 }
-initPayPalButton();
+try{initPayPalButton()}catch(err){}
+
 
