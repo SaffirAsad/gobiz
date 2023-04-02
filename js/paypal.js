36dfc37968f2168b92dc953c3746e1f7d59420d7
SaffirAsad <125597561+SaffirAsad@users.noreply.github.com>
2023-04-02 16:34:22 +0000

    b

--- js/paypal.js
+++ js/paypal.js
@@ -10,7 +10,7 @@ else {
 });
 
 function initPayPalButton() {
-var amount = document.querySelector('#mainTotalPrice').querySelector(".total").innerHTML;
+var amount = 100;// document.querySelector('#mainTotalPrice').querySelector(".total").innerHTML;
 paypal.Buttons({
     style: {
     shape: 'rect',
